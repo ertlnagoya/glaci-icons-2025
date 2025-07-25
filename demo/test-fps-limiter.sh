b950a14046e85afbe49a0c0dde13c30a96ad9338
YixiaoLi <liyixiao7@gmail.com>
2025-07-25 10:24:28 +0900

    add demo scripts

---
+++ demo/test-fps-limiter.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+function clean_up {
+    echo "Clean up"
+    kill $APP_PID
+}
+trap clean_up EXIT
+
+DISPLAY=:0 LD_PRELOAD=$PWD/../build/intercept.so glmark2 -b terrain:duration=60 -s 1920x1080 &
+APP_PID=$!
+echo glmark2 is started with pid [$APP_PID].
+
+# Delay several seconds for warm-up
+sleep 2
+
+sudo ./fps-limiter.py $APP_PID 60
